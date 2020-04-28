851e3041dd3e6105f8126bb6edd9fdc9339225a7
Jack.Gardner <minigunman1997@gmail.com>
2020-04-28 23:27:44 +1000

    Fixed date time issue on date

--- Models/QueryObjects/GameQuery.cs
+++ Models/QueryObjects/GameQuery.cs
@@ -5,7 +5,7 @@ namespace Banjo_kazooie_api.Models.QueryObjects
 {
     public class GameQuery: BaseQuery
     {
-        public DateTime Released { get; set; }
+        public DateTime? Released { get; set; }
         public string Platform { get; set; }
         public List<int> CharacterId { get; set; }
         public List<int> CollectableId { get; set; }
